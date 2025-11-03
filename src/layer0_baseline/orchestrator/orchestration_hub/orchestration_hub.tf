573ba9255778ae83c9a54aad1348af751f8626af
Dr. Rigoberto Garcia <riggarcia@teksystems.com>
2025-11-03 18:09:31 -0500

    Merged all updates from development into main  including orchestrator changes

--- src/layer0_baseline/orchestrator/orchestration_hub/orchestration_hub.tf
+++ src/layer0_baseline/orchestrator/orchestration_hub/orchestration_hub.tf
@@ -1,5 +1,5 @@
 # ======================================================================
 #  Created by: Dr. Rigoberto Garcia 10/22/2025
-#  TODO: orchestration hub configuration for Layer 0 Baseline.
+#  TODO: orchestration hub configuration for Layer 0 Baseline. 1
 #   
 # ======================================================================
