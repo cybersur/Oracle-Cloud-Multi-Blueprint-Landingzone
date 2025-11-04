a6c734d2ed1a5e74d96467321a9a7ed675ef3c8f
Dr. Rigoberto Garcia <riggarcia@teksystems.com>
2025-11-03 23:25:41 -0500

    Added Layer0 core-lz-local (IAM + Observability) and updated upstream modules for Layer01 demo

--- src/layer0_baseline/stacks/core_lz_baseline/variables.tf
+++ src/layer0_baseline/stacks/core_lz_baseline/variables.tf
@@ -17,35 +17,6 @@ variable "child_tenancy_ocid" {
 }
 
 variable "user_ocid" {
-  description = "OCID of your IAM user"
-  type        = string
-  default     = "ocid1.user.oc1..aaaaaaaafesuqya4bxszsohfndhw75ufcyuwor6bcaa7ogyahzjty24sq73a"
-}
-
-variable "fingerprint" {
-  description = "API key fingerprint"
-  type        = string
-  default     = "23:90:2b:2b:27:ab:da:51:eb:79:e7:ba:85:7f:2b:6d"
-}
-
-variable "private_key_path" {
-  description = "Path to your OCI API private key"
-  type        = string
-  default     = "C:\\Users\\Administrator\\.oci\\oci_api_key.pem"
-}
-variable "tenancy_ocid" {
-  description = "OCID of the parent tenancy (allegisgroup)"
-  type        = string
-  default     = "ocid1.tenancy.oc1..aaaaaaaaawhowwgqzjjqhmnhkjumnmlegqfcock3v2f64scsembcnuz32iq"
-}
-
-variable "child_tenancy_ocid" {
-  description = "OCID of the child tenancy (tgsv1)"
-  type        = string
-  default     = "ocid1.tenancy.oc1..aaaaaaaaelre24qykkdaengpbb2jcmfahlyg2zu32ywgtu2wbpo3fzf25cq"
-}
-
-variable "user_ocid" {
   description = "OCID of the IAM user"
   type        = string
   default     = "ocid1.user.oc1..aaaaaaaafesuqya4bxszsohfndhw75ufcyuwor6bcaa7ogyahzjty24sq73a"
